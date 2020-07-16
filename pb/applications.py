65d3d0efff865ef35e03955aefa103579b89ae18
root <root@localhost.localdomain>
2020-07-16 14:43:36 +0800

    003

--- pb/applications.py
+++ pb/applications.py
@@ -9,6 +9,7 @@ role   : 管理端 Application
 from websdk.application import Application as myApplication
 from pb.handlers.penson_handler import penson_urls
 from pb.handlers.plan_handler import penson_urls as  plan_urls
+from pb.handlers.doc_handler import doc_mg_urls
 
 class Application(myApplication):
     def __init__(self, **settings):
